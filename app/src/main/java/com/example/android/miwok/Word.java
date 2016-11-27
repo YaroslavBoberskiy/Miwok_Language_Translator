47b703e3dd52f0a094978a9933c4eb1e896f15a2
Yaroslav <yaroslav.boberskiy@gmail.com>
2016-11-27 12:21:32 +0200

    Added OnAudioFocusChangeListener Added Handling of audio when audio focus Gain, Lost, Lost Transient

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -9,17 +9,20 @@ public class Word {
 
     private String mMiwokTranslation;
     private String mDefaultTranslation;
+    private int mVoiceResourceId;
     private int mImageResourceId = HAS_NOT_INITIALIZED;
 
-    public Word(String mDefaultTranslation, String mMiwokTranslation) {
+    public Word(String mDefaultTranslation, String mMiwokTranslation, int mVoiceResourceId) {
         this.mMiwokTranslation = mMiwokTranslation;
         this.mDefaultTranslation = mDefaultTranslation;
+        this.mVoiceResourceId = mVoiceResourceId;
     }
 
-    public Word(String mDefaultTranslation, String mMiwokTranslation, int mImageResourceId) {
+    public Word(String mDefaultTranslation, String mMiwokTranslation, int mImageResourceId, int mVoiceResourceId) {
         this.mMiwokTranslation = mMiwokTranslation;
         this.mDefaultTranslation = mDefaultTranslation;
         this.mImageResourceId = mImageResourceId;
+        this.mVoiceResourceId = mVoiceResourceId;
     }
 
     public String getmMiwokTranslation() {
@@ -32,6 +35,20 @@ public class Word {
 
     public int getmImageResourceId() {
         return mImageResourceId;
+    }
+
+    public int getmVoiceResourceId() {
+        return mVoiceResourceId;
+    }
+
+    @Override
+    public String toString() {
+        return "Word{" +
+                "mMiwokTranslation='" + mMiwokTranslation + '\'' +
+                ", mDefaultTranslation='" + mDefaultTranslation + '\'' +
+                ", mVoiceResourceId=" + mVoiceResourceId +
+                ", mImageResourceId=" + mImageResourceId +
+                '}';
     }
 
     public boolean hasmImage() {
--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -19,6 +19,7 @@ public class WordAdapter extends ArrayAdapter<Word> {
     private static final String LOG_TAG = WordAdapter.class.getSimpleName();
 
     private int backgrountColor;
+    private View listItemView;
 
     public WordAdapter(Context context, List<Word> words, int backgrountColor) {
         super(context, 0, words);
@@ -27,19 +28,19 @@ public class WordAdapter extends ArrayAdapter<Word> {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        View listItemView = convertView;
+        listItemView = convertView;
         if (listItemView == null) {
             listItemView = LayoutInflater.from(getContext()).inflate(
                     R.layout.list_item, parent, false);
         }
 
-        Word currentWord = getItem(position);
+        final Word currentWord = getItem(position);
         int color = ContextCompat.getColor(getContext(),backgrountColor);
 
         TextView miwokTranslationTextView = (TextView) listItemView.findViewById(R.id.miwok_text_view);
         miwokTranslationTextView.setText(currentWord.getmMiwokTranslation());
         miwokTranslationTextView.setBackgroundColor(color);
-
+        
         TextView defaultTranslationTextView = (TextView) listItemView.findViewById(R.id.default_text_view);
         defaultTranslationTextView.setText(currentWord.getmDefaultTranslation());
         defaultTranslationTextView.setBackgroundColor(color);
